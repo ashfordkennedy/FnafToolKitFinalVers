d44c6b6f17351c79a93c4650a52cd390980701f8
JamesJordan-Pineapple <80774993+JamesJordan-Pineapple@users.noreply.github.com>
2023-07-30 15:32:05 +0100

    changes

---
+++ Assets/MeshWelder.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+/// <summary>
+/// code generated through chatGPT as an example
+/// </summary>
+public class MeshWelder : MonoBehaviour
+{
+    public float weldThreshold = 0.001f; // Adjust this value based on your requirements.
+
+    private void Start()
+    {
+        // Access the MeshFilter component
+        MeshFilter meshFilter = GetComponent<MeshFilter>();
+        Mesh mesh = meshFilter.mesh;
+
+        // Weld vertices and preserve UVs
+        WeldVertices(mesh);
+
+        // Recalculate normals and bounds for correct lighting and rendering
+        mesh.RecalculateNormals();
+        mesh.RecalculateBounds();
+    }
+
+    private void WeldVertices(Mesh mesh)
+    {
+        Vector3[] vertices = mesh.vertices;
+        Vector2[] uvs = mesh.uv;
+        int[] triangles = mesh.triangles;
+
+        // Create a dictionary to store unique vertices and their corresponding index
+        Dictionary<Vector3, int> uniqueVertices = new Dictionary<Vector3, int>();
+
+        // Loop through all vertices and check for duplicates
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            Vector3 vertex = vertices[i];
+            if (!uniqueVertices.ContainsKey(vertex))
+            {
+                // Add the vertex to the dictionary if it doesn't exist
+                uniqueVertices.Add(vertex, i);
+            }
+            else
+            {
+                // If a similar vertex already exists within the threshold distance, use the existing index
+                int existingIndex = uniqueVertices[vertex];
+
+                // Update UVs of the existing vertex by averaging with the current UV
+                Vector2 avgUV = (uvs[i] + uvs[existingIndex]) * 0.5f;
+                uvs[existingIndex] = avgUV;
+
+                // Replace vertex index in triangles with the existing index
+                triangles = ReplaceVertexIndex(triangles, i, existingIndex);
+            }
+        }
+
+        // Apply the welded vertices and UVs to the mesh
+        Vector3[] weldedVertices = new Vector3[uniqueVertices.Count];
+        Vector2[] weldedUVs = new Vector2[uniqueVertices.Count];
+        uniqueVertices.Keys.CopyTo(weldedVertices, 0);
+        uniqueVertices.Values.CopyTo(weldedUVs, 0);
+        mesh.vertices = weldedVertices;
+        mesh.uv = weldedUVs;
+        mesh.triangles = triangles;
+    }
+
+    private int[] ReplaceVertexIndex(int[] triangles, int oldIndex, int newIndex)
+    {
+        for (int i = 0; i < triangles.Length; i++)
+        {
+            if (triangles[i] == oldIndex)
+            {
+                triangles[i] = newIndex;
+            }
+        }
+        return triangles;
+    }
+}
--- Assets/Scripts/MapEditor_Scripts/Editor_Mouse.cs
+++ Assets/Scripts/MapEditor_Scripts/Editor_Mouse.cs
@@ -2,17 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.EventSystems;
-using UnityEngine.Events;
-
 using UnityEngine.InputSystem;
+using UnityEngine.Events;
+using System;
+using System.Threading.Tasks;
 public class Editor_Mouse : MonoBehaviour
 {
     public static Editor_Mouse instance;
-    public UnityEvent RightClick = new UnityEvent();
-    public UnityEvent LeftClick = new UnityEvent();
-    public UnityEvent MiddleClick = new UnityEvent();
-    public UnityEvent MouseUpdate = new UnityEvent();
+
+    [SerializeField] Camera _targetCamera;
+
+    public static UnityEvent LeftClick = new UnityEvent();
+    public static UnityEvent RightClick = new UnityEvent();
+    public static UnityEvent ShiftClick = new UnityEvent();
+    public static UnityEvent MouseUpdate = new UnityEvent();
+
     public UnityEvent LeftClickHold = new UnityEvent();
     public UnityEvent ShiftLeftClick = new UnityEvent();
 
@@ -22,17 +26,94 @@ public class Editor_Mouse : MonoBehaviour
     [SerializeField] Editor_MouseMode_Select mousemode_Select;
 
 
-    public void Awake()
+    public static UnityEvent Drag = new UnityEvent();
+    public static UnityEvent StopDrag = new UnityEvent();
+
+    public static UnityEvent UnassignMouseMode = new UnityEvent();
+
+    [SerializeField] public static GameObject MouseTarget;
+    [SerializeField] public static Vector3 MouseHitPos;
+    [SerializeField] public static Vector2 MousePos;
+
+    public float handOffset = 5f;
+    private bool dragging = false;
+    // Start is called before the first frame update
+    void Awake()
     {
         instance = this;
-        SetMouseMode(EditorMouseMode.Build);
     }
 
-    public void Update()
+    // Update is called once per frame
+    void Update()
     {
-        
     }
 
+    private void OnDrag(InputValue value)
+    {
+
+        switch (value.Get<float>())
+        {
+
+            case 1:
+                print("dragging");
+                dragging = true;
+                DragProcessing();
+                break;
+
+
+            case 0:
+                dragging = false;
+                StopDrag.Invoke();
+                print("drag end");
+                break;
+        }
+
+    }
+
+    private async void DragProcessing()
+    {
+
+        while (dragging != false)
+        {
+            Drag.Invoke();
+            await Task.Delay(1000);
+            await Task.Yield();
+        }
+
+        return;
+    }
+
+    private void OnDoubleLeftClick(InputValue value)
+    {
+        print("double clicked " + value.Get<float>());
+    }
+
+    void OnMouseMove(InputValue value)
+    {
+        // print("moving mouse update " + Time.time);
+
+        RaycastHit hit;
+        MousePos = value.Get<Vector2>();
+        Ray ray = _targetCamera.ScreenPointToRay(MousePos);
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+        {
+
+        }
+    }
+
+    void OnLeftClick(InputValue value)
+    {
+        LeftClick.Invoke();
+        print("Left click working");
+    }
+
+
+
+    void OnRightClick(InputValue value)
+    {
+        RightClick.Invoke();
+        print("Right click working");
+    }
 
     void OnPoint(InputValue value)
     {
@@ -48,12 +129,6 @@ public class Editor_Mouse : MonoBehaviour
 
     }
 
-    void OnRightClick(InputValue value)
-    {
-        RightClick.Invoke();
-        print("right click input");
-
-    }
 
     void OnLeftClickDrag(InputValue value)
     {
